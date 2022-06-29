53177b0f501efe371f503c7b4d644ff619f3e7ee
LauraMenzel <menzel-laura@gmx.de>
2022-06-29 15:42:18 +0200

    adding agaaain

--- solution.js
+++ solution.js
@@ -4,7 +4,7 @@ console.log(apples==oranges)
 console.log(apples>oranges)
 console.log(oranges>apples)
 console.log(apples<=oranges)
-const mangos = 5;
+const mangoes = 5;
 console.log(mangoes * apples)
 console.log(mangoes * apples > mangoes + oranges)
 console.log(apples - mangoes < oranges / mangoes)
