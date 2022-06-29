6f236f2b3f1c2dda27dc75df61bf46ecae51e4b2
LauraMenzel <menzel-laura@gmx.de>
2022-06-29 15:13:49 +0200

    adding first js exercise

---
+++ solution.js
@@ -0,0 +1,13 @@
+const apples = 20;
+const oranges = 30;
+console.log(apples==oranges)
+console.log(apples>oranges)
+console.log(oranges>apples)
+console.log(apples<=oranges)
+const mangos = 5;
+console.log(mangos * apples)
+console.log(mangos * apples > mangos + oranges)
+console.log(apples - mangos < oranges / mangos)
+console.log(mangos == apples == oranges)
+console.log(apples % mangos == oranges % mangos)
+console.log(mangos + apples > oranges - mangos)
